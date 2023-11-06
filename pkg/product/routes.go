6002299f032d2ecac7cc631d034b6c0b69140d71
mangoes-pc <manggala@dagangan.com>
2023-11-06 15:57:00 +0700

    product services

--- pkg/product/routes.go
+++ pkg/product/routes.go
@@ -1,1 +1,29 @@
 package order
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/wiryawan46/go-grpc-api-gateway/pkg/auth"
+	"github.com/wiryawan46/go-grpc-api-gateway/pkg/config"
+	"github.com/wiryawan46/go-grpc-api-gateway/pkg/product/routes"
+)
+
+func RegisterRoutes(r *gin.Engine, c *config.Config, authsvc *auth.ServiceClient) {
+	a := auth.InitAuthMiddleware(authsvc)
+
+	svc := &ServiceClient{
+		Client: InitServiceClient(c),
+	}
+
+	routes := r.Group("/product")
+	routes.Use(a.AuthRequired)
+	routes.POST("/", svc.CreateProduct)
+	routes.GET("/:id", svc.FindOne)
+}
+
+func (svc *ServiceClient) FindOne(ctx *gin.Context) {
+	routes.FindOne(ctx, svc.Client)
+}
+
+func (svc *ServiceClient) CreateProduct(ctx *gin.Context) {
+	routes.CreateProduct(ctx, svc.Client)
+}
