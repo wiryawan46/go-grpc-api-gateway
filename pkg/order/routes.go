03ea7313856e5ddd604ca942103c28c684fc6ece
mangoes-pc <manggala@dagangan.com>
2023-11-06 11:09:17 +0700

    order services

--- pkg/order/routes.go
+++ pkg/order/routes.go
@@ -1,1 +1,24 @@
 package order
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/wiryawan46/go-grpc-api-gateway/pkg/auth"
+	"github.com/wiryawan46/go-grpc-api-gateway/pkg/config"
+	"github.com/wiryawan46/go-grpc-api-gateway/pkg/order/routes"
+)
+
+func RegisterRouter(r *gin.Engine, c *config.Config, authSvc *auth.ServiceClient) {
+	a := auth.InitAuthMiddleware(authSvc)
+
+	svc := &ServiceClient{
+		Client: InitServiceClient(c),
+	}
+
+	routerGroup := r.Group("/order")
+	routerGroup.Use(a.AuthRequired)
+	routerGroup.POST("/", svc.CreateOrder)
+}
+
+func (svc *ServiceClient) CreateOrder(ctx *gin.Context) {
+	routes.CreateOrder(ctx, svc.Client)
+}
